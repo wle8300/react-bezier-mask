8948b93b87aa0d0d4d811d06d552402771f167cd
wle8300 <email@wle8300.com>
2018-11-19 18:49:28 -0800

    asdf

--- example/src/App.js
+++ example/src/App.js
@@ -5,10 +5,11 @@ import ReactBezierMask from 'react-bezier-mask'
 export default class App extends Component {
   render () {
     return (
-      <div>
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100vw', height: '100vh', background: 'black'}}>
         <ReactBezierMask>
           <img src="https://picsum.photos/300/?random" width="300" height="300" alt="placeholder"/>
         </ReactBezierMask>
+        <div style={{marginTop: '2rem', color: 'white'}}>refresh page to see a different image</div>
       </div>
     )
   }
